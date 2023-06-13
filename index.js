f38774b58be5467bf6b23564a8b1e3e25b386cfb
abigailmgarcia <abigailmg31@gmail.com>
2023-06-13 02:45:09 -0500

    imported markdown file and created array question format

--- index.js
+++ index.js
@@ -2,8 +2,38 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const generatorMarkdown = require('./Develop/utils/generateMarkdown')
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    message: "What is the title of your project?",
+    name: "Title"
+}, {
+    Type: "input",
+    message: "",
+    name: ""
+}, {
+    type: "input",
+    message: "",
+    name: ""
+}, {
+    type: "input",
+    message: "",
+    name: ""
+}, {
+    type: "input",
+    message: "",
+    name: ""
+}, {
+    type: "input",
+    message: "",
+    name: ""
+}, {
+    type: "input",
+    message: "",
+    name: ""
+}];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
